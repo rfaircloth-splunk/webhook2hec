a86208fa5e5d2b7b2da498aeda6d2a8eacaef63c
rfaircloth-splunk <rfaircloth@splunk.com>
2020-04-03 14:13:47 -0400

    working code

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.7
+WORKDIR /app
+
+RUN pip install gunicorn
+
+ADD requirements.txt /app/requirements.txt
+RUN pip install -r /app/requirements.txt
+COPY app.py /app
+
+EXPOSE 8000
+
+
+ENV PYTHONUNBUFFERED=1
+CMD ["gunicorn", "-b", "0.0.0.0:5000", "--workers", "2", "--threads", "4", "--worker-class", "gthread", "--worker-tmp-dir", "/dev/shm", "app:app"]
+
+# add app info as environment variables
+ARG GIT_COMMIT
+ENV GIT_COMMIT $GIT_COMMIT
+ARG BUILD_TIMESTAMP
+ENV BUILD_TIMESTAMP $BUILD_TIMESTAMP
---
+++ app.py
@@ -0,0 +1,41 @@
+#!python
+from flask import Flask, jsonify, request, abort
+import requests
+from splunk_http_event_collector import http_event_collector
+import logging
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1.0/hook-relay", methods=["POST"])
+@app.route("/passthehook", methods=["POST"])
+def relay():
+
+    http_event_collector_host = request.args.get("server", default="unknown")
+    http_event_collector_port = request.args.get("port", default="443")
+    http_event_collector_key = request.args.get("hec_token", default="unknown")
+    if http_event_collector_host == "unknown" or http_event_collector_key == "unknown":
+        abort(400)
+
+    hec = http_event_collector(
+        http_event_collector_key,
+        http_event_collector_host,
+        http_event_port=http_event_collector_port,
+    )
+    hec.popNullFields = True
+    hec.log.setLevel(logging.DEBUG)
+    hec_payload = {}
+    hec_event = {}
+
+    hec.index = request.args.get("index", default="main")
+    hec.sourcetype = request.args.get("sourcetype", default="webhook")
+
+    hec_event.update({"host": "relayhookserver"})
+    hec_event.update({"event": request.json})
+    hec_payload.update({"event": hec_event})
+    hec.sendEvent(hec_payload)
+    return hec_payload
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
