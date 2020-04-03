cdf8b37e5a545a9f6de7a85e2d0c01fbe0ab8540
rfaircloth-splunk <rfaircloth@splunk.com>
2020-04-03 14:36:48 -0400

    fix

--- app.py
+++ app.py
@@ -30,8 +30,7 @@ def relay():
     hec.index = request.args.get("index", default="main")
     hec.sourcetype = request.args.get("sourcetype", default="webhook")
     hec.host = "relayserver"
-    hec_event.update({"event": request.json})
-    hec_payload.update({"event": hec_event})
+    hec_payload.update({"event": request.json})
     hec.sendEvent(hec_payload)
     return hec_payload
 
